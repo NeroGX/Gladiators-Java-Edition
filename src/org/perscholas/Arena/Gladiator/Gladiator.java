b9a26452e34f9c0345b21713ddded3d7ab3250c5
Joseph Robinson <josephr1981@live.com>
2022-10-01 11:40:32 -0400

    Created the Gladiator class to make the gladiators with their own name and weapon.

---
+++ src/org/perscholas/Arena/Gladiator/Gladiator.java
@@ -0,0 +1,12 @@
+package org.perscholas.Arena.Gladiator;
+
+public class Gladiator {
+    public String name;
+    public String weapon;
+
+
+    public Gladiator(String name, String weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+}
